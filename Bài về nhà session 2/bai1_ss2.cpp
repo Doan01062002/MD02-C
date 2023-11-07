b116fe79a96013967ed482734bbf77ad149c2db8
Doan01062002 <147359759+Doan01062002@users.noreply.github.com>
2023-11-07 23:40:34 +0700

    Add files via upload
    Bài về nhà session 2

---
+++ Bài về nhà session 2/bai1_ss2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    //khai báo khởi và khởi tạo biến cạnh của hình vuông là a, chu vi là P1, diện tích là S1
+    float a, P1, S1;
+    P1 = 4*a, S1 = a*a;
+    // khai báo và khởi tạo biến chiều rộng và chiều dài hình chữ nhật là c,d và chu vi là P2, diện tích là S2
+    float c, d, P2, S2;
+    P2 = 2*(c+d), S2 = c*d;
+    //khai bảo và khởi tạo biến bán kính của hình tròn là r, chu vi là P3, diện tích là S3 và PI=3,14
+    float r, P3, S3, PI = 3,14;
+    P3 = 2*PI*r, S3 = PI*r*r
+}
